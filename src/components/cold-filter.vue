d13f82e1bc0ff2f06026679e41a792dffc18660b
EchoMinkki <minkkigao@gmail.com>
2024-09-09 19:24:01 +0800

    second view

---
+++ src/components/cold-filter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="cold_filter">
+        <input type="range" v-model="filterValue" min="0" :max="1" step="0.25" @input="handleColdFilter" />
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            filterValue:1,
+        }
+    },
+    methods:{
+        handleColdFilter(){
+            console.log("处理冷色滤镜")
+        }
+    }
+}
+</script>
+
+<style>
+.cold_filter input{
+    width: 35%;
+}
+</style>
